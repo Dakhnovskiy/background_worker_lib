daac118d5d25bf80b87272869e35eeeec9ea38f6
dezman <dmitriy_dakhnovskiy@mail.ru>
2017-12-26 23:29:52 +0300

    add worker

--- background_worker_lib/worker/worker.py
+++ background_worker_lib/worker/worker.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from redis import Redis
+from ..queue import Queue
+
 
 class Worker:
 
-    def __init__(self):
+    def __init__(self, connect: Redis, name_queue=None):
+        self.queue = Queue(connect=connect, name=name_queue)
+
+    def start(self):
         pass
---
+++ tests/test_worker.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from fixtures import redis_instance
+
+from background_worker_lib import Worker
+from background_worker_lib import Queue
+
+
+def test_create_worker(redis_instance):
+    worker = Worker(connect=redis_instance)
+
+
+def test_worker_instance_has_attribute_queue(redis_instance):
+    worker = Worker(connect=redis_instance)
+    assert hasattr(worker, 'queue')
+    assert isinstance(worker.queue, Queue)
+
+
+def test_worker_has_start_method():
+    assert hasattr(Worker, 'start')
